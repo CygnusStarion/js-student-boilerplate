4d6e7473c70ddcac6d198d4e312a1733a6ff1475
CygnusStarion <joshua.gray@student.swic.edu>
2019-09-07 20:19:45 -0500

    Add numbers from 1 to 10 using while loop

--- scripts.js
+++ scripts.js
@@ -1,14 +1,15 @@
 // Write a program that sums the numbers from 1 to 10.
 
-let counter = 1;
-let sum = 0;
+let counter = 1; //placeholder variable
+let sum = 0; //variable to keep track of sum
 
 // `<=` is  COMPARISON OPERATOR
 while (counter <= 10) {
+  //while counter is <= 10, then do this:
   sum += counter; // sum = sum + counter;
 
   // `+=` is an ASSIGNMENT OPERATOR
   counter += 1; // counter = counter + 1;
 }
 
-console.log(sum);
+console.log(sum); //logs the sum
